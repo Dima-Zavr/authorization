35d5dd4360346bfc46378f5835cdabe37e91bbca
Dima-Sosin <sosindima5@gmail.com>
2025-10-08 15:10:39 +0700

    add pageLayout + refactoring button and input

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Button } from "@components/Button/Button";
 import { Input } from "@components/Input/Input";
 
+import { PageLayout } from "./components/PageLayout/PageLayout";
+
 export const App = () => {
     return (
-    <>
-        <Input type="text" placeholder="Телефон" />
-        <Button>Войти</Button>
-    </>
+        <PageLayout>
+            <h2>Вход</h2>
+            <p>Введите номер телефона для входа<br/> в личный кабинет</p>
+            <Input type="text" placeholder="Телефон" />
+            <Button>Войти</Button>
+        </PageLayout>
     );
 };
